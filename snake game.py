152124243a1d2a0c57078149ea7ef9201145dcf2
trisha1121 <r24021trisha@dpsrkp.net>
2024-08-05 17:49:58 +0530

    Add files via upload

---
+++ snake game.py
@@ -0,0 +1,116 @@
+import pygame
+import random
+
+pygame.init()
+#Background Image
+back = pygame.image.load('background.jpeg')
+
+# Colors
+white = (255, 255, 255)
+red = (255, 0, 0)
+black = (0, 0, 0)
+
+# Creating window
+screen_width = 1000
+screen_height = 600
+gameWindow = pygame.display.set_mode((screen_width, screen_height))
+
+# Game Title
+pygame.display.set_caption("Snakes")
+pygame.display.update()
+
+# Background Image
+back = pygame.transform.scale(back,(screen_width, screen_height))
+
+# Game specific variables
+exit_game = False
+game_over = False
+snake_x = 45
+snake_y = 55
+velocity_x = 0
+velocity_y = 0
+
+food_x = random.randint(20, screen_width/2)
+food_y = random.randint(20, screen_height/2)
+score = 0
+init_velocity = 5
+snake_size = 15
+fps = 60
+
+# Load Sound
+eat_sound = pygame.mixer.Sound("Music.mp3")
+
+clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 55)
+
+
+def text_screen(text, color, x, y):
+    screen_text = font.render(text, True, color)
+    gameWindow.blit(screen_text, [x,y])
+
+
+def plot_snake(gameWindow, color, snk_list, snake_size):
+    for x,y in snk_list:
+        pygame.draw.rect(gameWindow, color, [x, y, snake_size, snake_size])
+
+
+snk_list = []
+snk_length = 1
+
+# Game Loop
+while not exit_game:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                velocity_x = init_velocity
+                velocity_y = 0
+
+            if event.key == pygame.K_LEFT:
+                velocity_x = - init_velocity
+                velocity_y = 0
+
+            if event.key == pygame.K_UP:
+                velocity_y = - init_velocity
+                velocity_x = 0
+
+            if event.key == pygame.K_DOWN:
+                velocity_y = init_velocity
+                velocity_x = 0
+
+    snake_x = snake_x + velocity_x
+    snake_y = snake_y + velocity_y
+
+    if abs(snake_x - food_x)<15 and abs(snake_y - food_y)<15:
+        # Play eat sound when food is eaten
+        eat_sound.play()
+        score +=1
+        food_x = random.randint(20, screen_width / 2)
+        food_y = random.randint(20, screen_height / 2)
+        snk_length +=5
+
+    gameWindow.blit(back, (0, 0))
+    pygame.display.flip()
+    text_screen("Score: " + str(score * 10), red, 5, 5)
+    pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
+
+
+    head = []
+    head.append(snake_x)
+    head.append(snake_y)
+    snk_list.append(head)
+
+    if len(snk_list)>snk_length:
+        del snk_list[0]
+
+    # pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
+    plot_snake(gameWindow, black, snk_list, snake_size)
+
+    pygame.display.update()
+    clock.tick(fps)
+
+pygame.quit()
+quit()
